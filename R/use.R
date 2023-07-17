f98cf8bc99e8dab5f39912455b297d1ba07ae7db
Jared Norman <jared.norman@uct.ac.za>
2023-07-17 10:59:01 +0200

    Initial commit with use_mt() function

---
+++ R/use.R
@@ -0,0 +1,23 @@
+#' Use \pkg{mt} in your project
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # Set up mt files in mt/ directory
+#' \dontrun{use_mt()}
+use_mt <- function() {
+  if (!fs::dir_exists('mt')) {
+    fs::dir_create('mt')
+  }
+  templateFnames <- c(
+    'config.yml',
+    'packages.R',
+    'disModel.R',
+    'disModel.xlsx',
+    'runModel.R'
+  )
+  for (fname in templateFnames) {
+    fs::file_touch(fs::path_join(c('mt',fname)))
+  }
+}
